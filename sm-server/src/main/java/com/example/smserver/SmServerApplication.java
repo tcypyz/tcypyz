0561a753dd9abe4c4b38ada21641375552f3daf8
maoshen5478 <361193645@qq.com>
2022-03-11 19:53:54 +0800

    init

---
+++ sm-server/src/main/java/com/example/smserver/SmServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.smserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmServerApplication.class, args);
+    }
+
+}
---
+++ sm-server/src/test/java/com/example/smserver/SmServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
