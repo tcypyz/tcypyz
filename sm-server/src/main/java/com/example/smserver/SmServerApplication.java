adee7f9d899ce75f4408b93abd61877f332308d9
Yin183 <63329980+Yin183@users.noreply.github.com>
2022-03-13 13:07:54 +0800

    fix:

--- sm-server/src/main/java/com/example/smserver/SmServerApplication.java
+++ sm-server/src/main/java/com/example/smserver/SmServerApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class SmServerApplication {
-
+/*水电费*/
     public static void main(String[] args) {
         SpringApplication.run(SmServerApplication.class, args);
     }
