4e070157a10d537613e7f6ec67691e6522e74854
maoshen5478 <361193645@qq.com>
2022-03-15 17:41:45 +0800

    feat: base

--- sm-server/src/main/java/com/example/smserver/dto/BaseDTO.java
+++ sm-server/src/main/java/com/example/smserver/dto/BaseDTO.java
@@ -1,10 +1,14 @@
 package com.example.smserver.dto;
 
+import lombok.Data;
+import lombok.ToString;
+
 /**
  * @description:
  * @author: zhh
  * @time: 2022/3/15
  */
-
+@ToString
+@Data
 public class BaseDTO {
 }
